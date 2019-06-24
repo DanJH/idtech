4d67a73bd99a019207e1b7a8bbde3cab7c6a5b12
Daniel Heidorn <daniel.j.heidorn@gmail.com>
2019-06-24 15:00:31 -0400

    Create testingInput.py

---
+++ testingInput.py
@@ -0,0 +1,18 @@
+# first we are going to create a variable
+# this variable will be what we will edit
+newString = "This is a test string, enter a name to remember"
+
+# will output the new string
+print(newString)
+
+# setting the variable to input() will allow you to input into the string
+newString = input()
+# it will wait until something is typed and pressed enter before moving on 
+
+# will output edited string
+# using a comma outputs multiple things with a space in between
+print(newString, "is learning Python")
+
+# we can make new variables and input into them using a prompt
+# this prompt is something that goes in between the () in input
+name = input("Insert some name to remember")
