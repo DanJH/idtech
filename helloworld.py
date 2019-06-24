0f3b67e56651915cf48b7d5f1766da770b1bfa49
Daniel Heidorn <daniel.j.heidorn@gmail.com>
2019-06-24 12:54:46 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+# This is a comment, using a # means that this entire line will be ignored
+
+# variables are formed or more specifically defined by creating a name and setting it equal to something:
+testVariable=123
+# otherwise they can be spread out like this! (this is preferred)
+newVariable = "Hi this is a variable, notice my quotation marks!"
+
+# we can make it print a message when ran using
+print(newVariable)
+# if we want to print text we must use quotation marks
+print("test stuff to print")
+
+# we can quit the program by making it
+exit()
